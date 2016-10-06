8257e3927dbc064dfc8252fd247d2d75a2f6fbe3
Cyclic3 <harlan.connor@gmail.com>
2016-10-06 16:18:40 +0100

    Interrupts now actually work, and, as a bonus, don't misalign and corrupt the stack!

--- sysroot/usr/include/kernel/interrupts.h
+++ sysroot/usr/include/kernel/interrupts.h
@@ -1,21 +1,106 @@
 #include <kernel/cpu.h>
-void CallKernel(char* function_name,unsigned int n_args,void** args)
+typedef struct 
 {
+    char * function_name;
+    unsigned int n_args;
+    void ** args;;
+}KernelCall;
+void PushAll()
+{
+    asm("push %eax");
+    asm("push %ecx");
+    asm("push %edx");
+    asm("push %ebx");
+    //asm("push %esp");
+    //    asm("push %ebp");
+    //    asm("push %esi");
+    //    asm("push %edi");
+}
+void PopAll()
+{
+    //    asm("pop %edi");
+    //asm("pop %esi");
+    //    asm("pop %ebp");
+    //    asm("pop %esp");
+    asm("pop %ebx");
+    asm("pop %edx");
+    asm("pop %ecx");
+    asm("pop %eax");
+}
+void stackdump()
+{
+    //this function will screw everything up, but that won't matter
+    int esp,ebp;
+    asm("mov %%esp,%0;mov %%ebp,%1"::"r"(esp),"r"(ebp));
+    dump(esp,ebp);
+}
+size_t get_eip();
+asm(
+    "get_eip:"
+    "pop %eax;"
+    "push %eax;"
+    "push %eax;"
+    "ret"
+    );
+void volatile CallKernel(KernelCall * v)
+{
+    puts("CALL");
+    //printf("%x\n",v);
     //TODO: address translation
     //asm("push %0"::"r"((size_t)function_name));
-    asm("push %0;int $0x80"::"r"(n_args));
-    //asm("push %0"::"r"(args));
-    asm("int $0x80");
+    PushAll();
+    asm(""::"d"(v));
+    //
+
+    //asm("push %0"::"r"(args))
+    //asm("push 0x69686968");
+    //stackdump();
+    //size_t eip = get_eip();
+    //printf("faulty instruction @ %x\n",eip);
+    //asm("push %0"::"r"(eip));
+    asm("int $0x80;");//pop %ax");
+    //eip = get_eip();
+    //printf("faulty instruction @ %x\n",eip);
+    //dump(eip,eip+0x40);
+    //stackdump();
+    
+    PopAll();
+    //asm("pop %eax");
+    return;
+    //It is crashing between the return and the next instruction. This is probably due to a cirrupted stack pointer, and it failing top pop off the pointer
+    //asm("ljmp kernel_ret");
+    //PopAll();
 }
-void KernelCallIRQ()
+//void volatile KernelCallIRQ();
+void volatile KernelCallIRQ();
+asm(
+  "KernelCallIRQ:;"
+    //    "pushal;"
+    //  "cld;"
+      "call KernelCallIRQ_2;"
+    //  "call LOL;"
+    //    "popal;"
+    "ret;"
+  ";"
+    );
+void LOL() {puts("LOL");}
+void volatile KernelCallIRQ_2()
 {
-    char* function_name;
-    unsigned int n_args;
-    size_t args;
-    //asm volatile ("pop %0":"=r"(function_name));
-    asm volatile ("pop %0":"=r"(n_args));
-    //;pop %1;pop %0":"=r"(function_name),"=r"(n_args),"=r"(args));
-    printf("%x",n_args);
-    //;pop %1;pop %2","=X"(n_args),"=X"(args));
-    //I know the code above is evil, but it is the best that can be done.
+    
+    size_t v1;
+    asm("mov %%edx,%0":"=a"(v1));
+    KernelCall * v = (KernelCall *) v1;
+    unsigned char inline check(char * name,unsigned int n_args)
+    {
+        return (!strcmp(v->function_name,name))&&(v->n_args==n_args);
+    }
+    //puts(v->function_name);
+    printf("%s, %i\n",v->function_name,v->n_args);
+    if(check("puts",1))
+    {
+        puts((v->args));
+    }
+    else{puts("Bad kernel call");}
+    puts("ret");
+    return;
 }
